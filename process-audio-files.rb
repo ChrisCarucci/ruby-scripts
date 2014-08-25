86cb4b19732cfa72b3a3f09d21f8338cff71de99
Jarrett Coggin <jarrettcoggin@gmail.com>
2014-08-24 22:01:09 -0700

    Adding in ability to copy files to new location.

--- process-audio-files.rb
+++ process-audio-files.rb
@@ -6,6 +6,33 @@ This is a script that automatically converts a directory of WAV files and tags t
 
 require 'optparse'
 
+class Recording
+  def initialize(source_path)
+    @file_name = File.basename(source_path)
+    parts = @file_name.split(' - ')
+    @source_path = source_path
+    @type = parts.first
+    @year = File.ctime(source_path).to_s.split('-').first
+    @description = parts.last.chomp(".WAV")
+    @converted = false
+  end
+
+  def convert
+    title = (@type == "ZTL") ? "Market Research - " + @description : "ISD Session - " + @description
+    album = (@type == "ZTL") ? "Zero To Launch" : "Invisible Script Demolishers"
+    lame_command = "lame -V6 --tt '#{title}' --ta 'Jarrett Coggin' --tl '#{album}' --ty '#{@year}' #{@source_path.gsub(' ', '\ ')}"
+    puts lame_command
+    `#{lame_command}`
+    @converted = true
+  end
+
+  def move(destination)
+    if @converted
+      FileUtils.mv @source_path.gsub('WAV','mp3').gsub(' ', '\ ') destination+@file_name.gsub('WAV','mp3').gsub(' ', '\ ')
+    end
+  end
+end
+
 options = {}
 OptionParser.new do |opts|
   opts.banner = "Usage: process-audio-files.rb -s foldername"
@@ -19,9 +46,7 @@ if folder != nil
 
   #Process each file in the directory, pulling out the filename and using that to define where the file will go and what parameters to pass to LAME.
   files.each do |file|
-    filename = File.basename(file)
-    parts = filename.split('-')
-    type = parts[0]
-    # TODO: add in date extraction and description extraction.
+    recording = Recording.new(file)
+    recording.convert
   end
 end
